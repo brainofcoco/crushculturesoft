0dc24d4428560d3b6e0732de1ad0dcb86f7f5576
Pinza studio <67124011+dev-pinza@users.noreply.github.com>
2021-11-10 23:38:18 +0100

    Push online to see

--- demo_request.php
+++ demo_request.php
@@ -14,32 +14,80 @@ $feedback = "";
           <div class="row">
               <?php
               // email
-if(isset($_POST['submit'])){
-    $to = "erpdemo@thecrushculture.com"; // this is your Email address
+if(isset($_POST['submit'])) {
+    // run email
+    require 'vendor/autoload.php';
+
+// Without Composer (and instead of "require 'vendor/autoload.php'"):
+// require("your-path/sendpulse-rest-api-php/src/ApiInterface.php");
+// require("your-path/sendpulse-rest-api-php/src/ApiClient.php");
+// require("your-path/sendpulse-rest-api-php/src/Storage/TokenStorageInterface.php");
+// require("your-path/sendpulse-rest-api-php/src/Storage/FileStorage.php");
+// require("your-path/sendpulse-rest-api-php/src/Storage/SessionStorage.php");
+// require("your-path/sendpulse-rest-api-php/src/Storage/MemcachedStorage.php");
+// require("your-path/sendpulse-rest-api-php/src/Storage/MemcacheStorage.php");
+
+use Sendpulse\RestApi\ApiClient;
+use Sendpulse\RestApi\Storage\FileStorage;
+
+// API credentials from https://login.sendpulse.com/settings/#api
+define('API_USER_ID', '9b92c7fa94a367f8eba33df1a3227223');
+define('API_SECRET', 'fb9ca21572268b0b6e5f33bd43a8b105');
+define('PATH_TO_ATTACH_FILE', __FILE__);
+
+$SPApiClient = new ApiClient(API_USER_ID, API_SECRET, new FileStorage());
+
+/*
+ * Example: Send mail using SMTP
+ */
+$to = "erpdemo@thecrushculture.com"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
     $first_name = $_POST['name'];
     $phone = $_POST['phone'];
-    $subject = "Crush Culture ERP Demo";
-    $subject2 = "Copy of your form submission";
-    $message = $first_name . " " . $phone . " Company:" . "\n\n" . $_POST['company']
-    . " Website:" . "\n\n" . $_POST['website']
-    . " Country:" . "\n\n" . $_POST['country']
-    . " Address:" . "\n\n" . $_POST['address']
-    . " Message:" . "\n\n" . $_POST['post-comment']
-    ;
-    $message2 = "Here is a copy of your message " . $first_name . " " . $phone . " Company:" . "\n\n" . $_POST['company']
-    . " Website:" . "\n\n" . $_POST['website']
-    . " Country:" . "\n\n" . $_POST['country']
-    . " Address:" . "\n\n" . $_POST['address']
-    . " Message:" . "\n\n" . $_POST['post-comment']
-    ;
+    $website = $_POST['website'];
+    $company = addslashes($_POST['company']);
+    $country = addslashes($_POST['country']);
+    $address = addslashes($_POST['address']);
+    $post_comment = addslashes($_POST['post-comment']);
 
-    $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
-    mail($to,$subject,$message,$headers);
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
+
+
+$email = array(
+    'subject' => '<p>Hello!</p>',
+    'template' => array(
+        'id' => 'DemoBooking',
+        'variables' => {
+          'name' => $first_name,
+          'phone' => $phone,
+          'company' => $company,
+          'country' => $country,
+          'address' => $address,
+          'post_comment' => $post_comment,
+          'current_year' => date('Y')
+        }
+    ),
+    'from' => array(
+        'name' => 'Crush Culture ERP',
+        'email' => $to,
+    ),
+    'to' => array(
+        array(
+            'name' => $first_name,
+            'email' =>  $from,
+        ),
+    ),
+    'bcc' => array(
+        array(
+            'name' => 'Oluwaseun Samuel',
+            'email' => 'sunnyboye2015@gmail.com',
+        ),
+    ),
+    'attachments' => array(
+        'file.txt' => file_get_contents(PATH_TO_ATTACH_FILE),
+    ),
+);
+var_dump($SPApiClient->smtpSendMail($email));
+
     $feedback = "We got your message contact you soon!";
     }
               ?>
@@ -47,7 +95,7 @@ if(isset($_POST['submit'])){
               <div class="comment-wrap style1">
                 <h3>Try it for Free </h3>
                 <p>Book a crush culture demo session, a personal login credential will be sent to and will be valid for 24hrs, feel free to also contact us</p>
-                <form class="comment-form" method="post" action="#">
+                <form class="comment-form" method="post">
                   <input type="text" name="name" placeholder="Full name" required="">
                   <input type="email" name="email"  placeholder="Email" required="">
                   <input type="text" name="phone" placeholder="Phone">
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '613b0f760f69f2800766522d975393d4d4bfa017',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '613b0f760f69f2800766522d975393d4d4bfa017',
+            'dev_requirement' => false,
+        ),
+        'sendpulse/rest-api' => array(
+            'pretty_version' => '1.0.26',
+            'version' => '1.0.26.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../sendpulse/rest-api',
+            'aliases' => array(),
+            'reference' => 'e20b957c9b3e88563f449622c1f963f81e47c2d8',
+            'dev_requirement' => false,
+        ),
+    ),
+);
